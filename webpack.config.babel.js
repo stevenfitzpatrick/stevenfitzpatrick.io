f98a705cb346700b4523dd85faa2d62ec70424e5
Steven Fitzpatrick <steven.fitzpatrick1985@gmail.com>
2017-01-15 20:42:53 +0000

    Adding V8LazyParseWebpackPlugin Lib

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,11 @@
 // Modules
+
 import webpack from 'webpack';
+
 import path from 'path';
+
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 // Import Prod Settings
@@ -9,92 +13,80 @@ import prodPlugins from './webpack.prod.config.babel';
 
 // Environment Variables
 const ENV = process.env.npm_lifecycle_event;
+
 const isProd = ENV === 'build';
 
 module.exports = {
-    context: path.resolve(__dirname),
-    devServer: {
-        outputPath: path.join(__dirname, 'src'),
-        contentBase: './src',
-        open: true,
-        historyApiFallback: true
-    },
-    // Start Bundling Here
-    entry: {
-        app: './src/index.js'
-    },
-    // Output of Bundling
-    /* 
-    [path] - Returns entry path
-    [name] - Returns entry Name
-    [hash] - Returns the build hash
-    [chunkhash] - Retuns Chunk specific hash
-    */
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: isProd ? '' : 'http://localhost:8080/',
-        filename: '[name].bundle.js',
-        chunkFilename: '[chunkhash].js'
-    },
-    resolve: {
-        modules: [
-            'node_modules',
-            path.resolve(__dirname, 'node_modules'),
-            path.resolve(__dirname, 'src'),
-        ],
-        extensions: ['.js', '.json', '.jsx', '.css']
-    },
-    target: 'web',
-    // eval for dev, source-map for production       
-    devtool: isProd ? '' : 'source-map',
-    // Transform Rules
-    module: {
-        rules: [
-            // JS Loader
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    compact: true
-                }
-                //include: PATHS.style
-            },
-            // CSS Loader
-            {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader!postcss-loader'
-            },
-            // HTML Files
-            {
-                test: /\.html$/,
-                loader: 'html-loader'
-            },
-
-        ]
-    },
-    // Bundle Rules
-    plugins: ([
-        // Inject CSS and JS into HTML
-        new HtmlWebpackPlugin({
-            hash: false,
-            filename: 'index.html',
-            template: './src/index.html',
-            inject: true,
-            minify: {
-                removeComments: true,
-                collapseWhitespace: false
-            }
-        }),
-        // Write out CSS bundle to its own file:
-        new ExtractTextPlugin({
-            filename: 'css/[name].styles.[contenthash].css',
-            allChunks: true
-        }),
-        // Set Environment Variables
-        new webpack.DefinePlugin({
-            'process.env.environment': isProd ? JSON.stringify('prod') : JSON.stringify('dev')
-        })
-    ]).concat(isProd ? prodPlugins : [])
+  context: path.resolve(__dirname),
+  devServer: {
+    outputPath: path.join(__dirname, 'src'),
+    contentBase: './src',
+    open: true,
+    historyApiFallback: true
+  },
+  // Start Bundling Here
+  entry: { app: './src/index.js' },
+  // Output of Bundling
+  /* 
+        [path] - Returns entry path
+        [name] - Returns entry Name
+        [hash] - Returns the build hash
+        [chunkhash] - Retuns Chunk specific hash
+        */
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: isProd ? '' : 'http://localhost:8080/',
+    filename: '[name].bundle.js',
+    chunkFilename: '[chunkhash].js'
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(__dirname, 'src')
+    ],
+    extensions: [ '.js', '.json', '.jsx', '.css' ]
+  },
+  target: 'web',
+  // eval for dev, source-map for production       
+  devtool: isProd ? 'source-map' : 'eval',
+  // Transform Rules
+  module: {
+    rules: [
+      // JS Loader
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        //include: PATHS.style
+        options: { compact: true }
+      },
+      // CSS Loader
+      { test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader' },
+      // HTML Files
+      { test: /\.html$/, loader: 'html-loader' }
+    ]
+  },
+  // Bundle Rules
+  plugins: [
+    // Inject CSS and JS into HTML
+    new HtmlWebpackPlugin({
+      hash: false,
+      filename: 'index.html',
+      template: './src/index.html',
+      inject: true,
+      minify: { removeComments: true, collapseWhitespace: false }
+    }),
+    // Write out CSS bundle to its own file:
+    new ExtractTextPlugin({
+      filename: 'css/[name].styles.[contenthash].css',
+      allChunks: true
+    }),
+    // Set Environment Variables
+    new webpack.DefinePlugin({
+      'process.env.environment': isProd
+        ? JSON.stringify('prod')
+        : JSON.stringify('dev')
+    })
+  ].concat(isProd ? prodPlugins : [])
 };
-
--- webpack.prod.config.babel.js
+++ webpack.prod.config.babel.js
@@ -1,72 +1,72 @@
 import webpack from 'webpack';
+
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const BundleAnalyzerPlugin = require(
+  'webpack-bundle-analyzer'
+).BundleAnalyzerPlugin;
+
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+
+const V8LazyParseWebpackPlugin = require('v8-lazy-parse-webpack-plugin');
+
 const pkg = require('./package.json');
 
 const prodPlugins = [
-    // Don't Finish if any issues occur
-    new webpack.NoErrorsPlugin(),
-    // Todo check the options
-    new webpack.optimize.CommonsChunkPlugin({
-        name: 'app'
-    }),
-    // Specific Options for Loading
-    new webpack.LoaderOptionsPlugin({
-        minimize: true,
-        debug: false
-    }),
-    // Minimize JS
-    new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            warnings: false,
-            screw_ie8: true,
-            conditionals: true,
-            unused: true,
-            comparisons: true,
-            sequences: true,
-            dead_code: true,
-            evaluate: true,
-            if_return: true,
-            join_vars: true
-        },
-        output: {
-            comments: false
-        },
-        sourceMap: false,
-        minimize: true
-    }),
-    // Clean up dist folder after each build
-    new CleanWebpackPlugin(['dist'], {
-        root: __dirname,
-        verbose: true,
-        dry: false
-    }),
-    // Add Service Worker
-    new SWPrecacheWebpackPlugin(
-        {
-            cacheId: pkg.name,
-            filename: 'sw.js',
-            maximumFileSizeToCacheInBytes: 4194304,
-            stripPrefixMulti: {
-                'C:/dev/stevenfitzpatrick.io/dist': '',
-                '/home/travis/build/stevenfitzpatrick/stevenfitzpatrick.io/dist': ''
-            }
-        }
-    ),
-    // Add Bundle JS Analyzer
-    new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
-        openAnalyzer: true,
-        generateStatsFile: false,
-        reportFilename: '../src/report.html',
-    }),
-    // Move Files
-    new CopyWebpackPlugin([
-        { from: 'src/manifest.json' },
-    ]),
+  // Don't Finish if any issues occur
+  new webpack.NoErrorsPlugin(),
+  // Todo check the options
+  new webpack.optimize.CommonsChunkPlugin({ name: 'app' }),
+  // Specific Options for Loading
+  new webpack.LoaderOptionsPlugin({ minimize: true, debug: false }),
+  // Use exploit the V8/Chakra engines treatment of functions, this lazy loads the parsing
+  new V8LazyParseWebpackPlugin(),
+  // Minimize JS
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+      screw_ie8: true,
+      conditionals: true,
+      unused: true,
+      comparisons: true,
+      sequences: true,
+      dead_code: true,
+      evaluate: true,
+      if_return: true,
+      join_vars: true,
+      negate_iife: false
+    },
+    output: { comments: false },
+    sourceMap: false,
+    minimize: true
+  }),
+  // Clean up dist folder after each build
+  new CleanWebpackPlugin([ 'dist' ], {
+    root: __dirname,
+    verbose: true,
+    dry: false
+  }),
+  // Add Service Worker
+  new SWPrecacheWebpackPlugin({
+    cacheId: pkg.name,
+    filename: 'sw.js',
+    maximumFileSizeToCacheInBytes: 4194304,
+    stripPrefixMulti: {
+      'C:/dev/stevenfitzpatrick.io/dist': '',
+      '/home/travis/build/stevenfitzpatrick/stevenfitzpatrick.io/dist': ''
+    }
+  }),
+  // Add Bundle JS Analyzer
+  new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    openAnalyzer: true,
+    generateStatsFile: false,
+    reportFilename: '../src/report.html'
+  }),
+  // Move Files
+  new CopyWebpackPlugin([ { from: 'src/manifest.json' } ])
 ];
 
 export default prodPlugins;
