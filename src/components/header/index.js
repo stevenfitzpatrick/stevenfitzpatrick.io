c08e7d3854ffe4e0549da49433a143663eca86f3
Steven Fitzpatrick <steven.fitzpatrick1985@gmail.com>
2017-01-19 21:12:47 +0000

    Playing withing Next Css

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,16 +1,15 @@
 import { h, Component } from 'preact';
-
+import style from './style';
 import { Link } from 'preact-router';
 
 export default class Header extends Component {
   render() {
     return (
       <header>
-        <h1>Checking Header</h1>
         <nav>
-          <Link href='/'>Home</Link>
-          <Link href='/about'>About</Link>
-          <Link href='/favourites'>Favourites</Link>
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/favourites">Favourites</Link>
         </nav>
       </header>
     );
