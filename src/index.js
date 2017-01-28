8ce5aab4dafd0a6b12333ff0f4689acc9e951020
Steven Fitzpatrick <steven.fitzpatrick1985@gmail.com>
2017-01-28 22:11:33 +0000

    Fix Tests

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
   });
 
   //Add Google Analytics
+  /*global ga*/
   (function (i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
       (i[r].q = i[r].q || []).push(arguments)
