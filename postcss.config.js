0879607991ef4db417a3c52a406fe08b0a042332
Steven Fitzpatrick <steven.fitzpatrick1985@gmail.com>
2017-02-26 19:02:41 +0000

    Fix Build

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,8 @@
 const webpack = require('webpack');
 
 module.exports = {
-    plugins: [
-        require('postcss-import')({ addDependencyTo: webpack }),
-        require('postcss-url')(),
-        require('postcss-cssnext')()
-    ]
+  plugins: [
+    require('postcss-import')({ addDependencyTo: webpack }),
+    require('postcss-url')()
+  ]
 };
-
