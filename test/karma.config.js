c150e5179fa195e5d20844eb44ae614a63f32d64
Steven Fitzpatrick <steven.fitzpatrick1985@gmail.com>
2017-04-07 19:23:56 +0100

    Another Travis Test Attempt Fix

--- test/karma.config.js
+++ test/karma.config.js
@@ -5,12 +5,11 @@ const webpack = require('../webpack.config.babel.js');
 module.exports = function(config) {
   config.set({
     basePath: '../',
-    frameworks: [ 'mocha', 'chai-sinon' ],
-    browsers: [ 'PhantomJS' ],
-    files: [ 'test/browser/**/*.js' ],
+    frameworks: ['mocha', 'chai-sinon'],
+    browsers: ['PhantomJS'],
+    files: ['test/browser/**/*.js'],
     preprocessors: {
-      'test/**/*.js': [ 'webpack' ],
-      'src/**/*.js': [ 'webpack' ]
+      'test/**/*.js': ['webpack']
     },
     webpack,
     webpackMiddleware: { noInfo: true }
