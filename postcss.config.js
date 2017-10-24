15e951787af088fc98bbe289c1afaae2337fadc8
Steven Fitzpatrick <steven.fitzpatrick1985@gmail.com>
2017-10-24 22:36:44 +0100

    Moving up latest changes

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,9 @@ module.exports = {
   plugins: [
     require('postcss-import')({ addDependencyTo: webpack }),
     require('postcss-url')(),
-    require('autoprefixer')()
+    require('autoprefixer')(),
+    require('cssnano')({
+      preset: 'default'
+    })
   ]
 };
