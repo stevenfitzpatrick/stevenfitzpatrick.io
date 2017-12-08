b75038b47483135453835841f4c4240d783e9f32
Steven Fitzpatrick <steven.fitzpatrick1985@gmail.com>
2017-12-08 21:05:20 +0000

    Stylelint and husky

--- src/styles/buttons.js
+++ src/styles/buttons.js
@@ -2,17 +2,17 @@ import styled from 'styled-components';
 
 export const FunkyH5 = styled.h5`
   line-height: 1;
-  margin: 0 0 24px 0;
-  position: relative;
-  font-weight: 600;
+margin: 0 0 24px 0;
+position: relative;
+font-weight: 600;
 
-  &::after {
-    content: '';
-    position: absolute;
-    bottom: -5px;
-    left: 0;
-    width: 1em;
-    height: 2px;
-    background-color: #d28546;
-  }
+&::after {
+  content: '';
+  position: absolute;
+  bottom: -5px;
+  left: 0;
+  width: 1em;
+  height: 2px;
+  background-color: #d28546;
+}
 `;
